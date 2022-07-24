1deae7a458434c9f627c820a44eb364a052c1ce4
Valentine <mselinkor@gmail.com>
2022-07-24 23:20:47 +0300

    added SuperPower feature

---
+++ ExoftMiniGame/SuperPowers.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Warriors;
+
+namespace SuperPower
+{   
+    internal class SuperHealth : ISuperPower
+    {   
+        public void AddSuperPower(Warrior warrior, int power)
+        {
+            warrior.HP += power;
+        }
+    }
+    internal class SuperArmor : ISuperPower
+    {
+        public void AddSuperPower(Warrior warrior, int power)
+        {
+            warrior.Armor += power;
+        }
+    }
+    internal class SuperAttack : ISuperPower
+    {
+        public void AddSuperPower(Warrior warrior, int power)
+        {
+            warrior.Attack += power;
+        }
+    }
+}
+
