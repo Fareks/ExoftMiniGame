fdac2746391d96c4c54ec9b29343acd345825e10
Valentine <mselinkor@gmail.com>
2022-07-24 18:12:50 +0300

    add Warrior/ Warriors Classes

--- ExoftMiniGame/Program.cs
+++ ExoftMiniGame/Program.cs
@@ -1,3 +1,3 @@
 ﻿using SuperPower;
-
+using Warrior;
 
---
+++ ExoftMiniGame/Warrior.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Warrior
+{
+    abstract class Warrior
+    {
+        protected int hp = 100;
+        protected int armor;
+        protected int attack;
+
+        void setAttribute(int armor,int attack)
+        {
+            this.armor = armor;
+            this.attack = attack;
+        }
+        public int getAttack ()
+        {
+            return armor;
+        }
+
+    }
+
+    internal class Archer : Warrior
+    {
+        int armor = 8;
+        int attack = 12;
+    }
+
+    internal class Swordman : Warrior
+    {
+        int armor = 10;
+        int attack = 10;
+    }
+
+    internal class Paladin : Warrior
+    {
+        int armor = 15;
+        int attack = 5;
+    }
+
+    internal class Mage : Warrior
+    {
+        int armor = 5;
+        int attack = 15;
+    }
+}
