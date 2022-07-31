6af9add211b6bbc033fd36ec999419ec5fe4b155
Valentine <mselinkor@gmail.com>
2022-07-31 23:37:59 +0300

    added new methods
    new:created method - StartAllBattles (max battles: 100), created method - return List of heroes GetAllHeroes

--- ExoftMiniGame/BattleHelper.cs
+++ ExoftMiniGame/BattleHelper.cs
@@ -8,6 +8,21 @@ namespace BattleHelpers
 {
     public static class BattleHelper
     {
+
+        public static void HeroesMenu()
+        {
+            List<Warrior> heroes = new List<Warrior>();
+            Console.WriteLine("Please, choose num pairs of fighters (example: 2 pairs = 4 fighters)");
+            int num =  int.Parse(Console.ReadLine());
+            for (int i=0; i<num; i++)
+            {
+                heroes.Add(BattleHelper.GenerateHero("Player 1, choose your hero: 1. Archer, 2. Swordman, 3. Paladin, 4. Mage, 5. Morphling"));
+            }
+            //Warrior player_1 = BattleHelper.GenerateHero("Player 1, choose your hero: 1. Archer, 2. Swordman, 3. Paladin, 4. Mage, 5. Morphling");
+            //Warrior player_2 = BattleHelper.GenerateHero("Player 2, choose your hero: 1. Archer, 2. Swordman, 3. Paladin, 4. Mage, 5. Morphling");
+            //Warrior player_3 = BattleHelper.GenerateHero("Player 3, choose your hero: 1. Archer, 2. Swordman, 3. Paladin, 4. Mage, 5. Morphling");
+            //Warrior player_4 = BattleHelper.GenerateHero("Player 4, choose your hero: 1. Archer, 2. Swordman, 3. Paladin, 4. Mage, 5. Morphling");
+        }
         public static Warrior GenerateHero(string question)
         {
             Warrior hero;
--- ExoftMiniGame/Program.cs
+++ ExoftMiniGame/Program.cs
@@ -7,11 +7,8 @@ SuperPowerGenerator superPowerGenerator = new SuperPowerGenerator();
 SuperArmor superArmor = new SuperArmor();
 
 
-Warrior player_1 = BattleHelper.GenerateHero("Player 1, choose your hero: 1. Archer, 2. Swordman, 3. Paladin, 4. Mage, 5. Morphling");
-Warrior player_2 = BattleHelper.GenerateHero("Player 2, choose your hero: 1. Archer, 2. Swordman, 3. Paladin, 4. Mage, 5. Morphling");
-Warrior player_3 = new Archer();
-Warrior player_4 = new Paladin();
-player_2.AddSuperPower(superArmor, superPowerGenerator.getSuperPower());
+
+player_3.AddSuperPower(superArmor, superPowerGenerator.getSuperPower());
 player_1.AddSuperPower(superArmor, superPowerGenerator.getSuperPower());
 player_1.AddHP(5);
 Console.WriteLine($" Prepare for battle! {player_1.name} VS {player_2.name}");
